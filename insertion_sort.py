49fd0b703324872cb94cdb9eb0475ad7016ccc17
CrunchyPistacho <40769951+CrunchyPistacho@users.noreply.github.com>
2020-01-03 22:50:25 +0100

    add insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,16 @@
+# imput array
+
+A = [5,3,4,2,1,10,8,15,22,6]
+
+def insertion_sort(x):
+    for j in range(1,len(x)):
+        key = x[j]
+        i = j - 1
+        while i >= 0 and x[i] > key:
+            x[i + 1] = x[i]
+            i = i - 1
+        x[i + 1] = key
+    return(x)
+
+
+print(insertion_sort(A))
