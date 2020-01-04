d7a8fd3b630de3064aa8c36c3bfc66871410a15b
CrunchyPistacho <40769951+CrunchyPistacho@users.noreply.github.com>
2020-01-04 11:35:44 +0100

    ad selection sort

---
+++ sorting_algorithms/selection_sort.py
@@ -0,0 +1,14 @@
+
+A = [5,3,4,2,1,10,8,15,22,6]
+
+def selection_sort(x):
+    for j in range(0,len(x)):
+        key = j
+        for i in range(j,len(x)):
+            if x[key] > x[i]:
+                key = i
+        x[j], x[key] = x[key], x[j]
+    return(x)
+
+
+print(selection_sort(A))
